3fb1937152ee0100517e1599c10dbdb78d4591c0
shahriar6740 <hasan.shahriar@ideascale.com>
2022-09-12 17:33:35 +0600

    initial commint for both front and backend

---
+++ backend/todo/models.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from pydoc import describe
+from pyexpat import model
+from statistics import mode
+from django.db import models
+
+# Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
